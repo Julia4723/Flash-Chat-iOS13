563f2eafff056dc54506d058cc3ffb8a036c2809
My <uk472363@yanex.ru>
2024-10-17 18:02:03 +0300

    Final commit

---
+++ Flash Chat iOS13/K.swift
@@ -0,0 +1,15 @@
+//
+//  Constatns.swift
+//  Flash Chat iOS13
+//
+//  Created by user on 14.10.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+
+
+struct K {
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+}
---
+++ Flash Chat iOS13/Views/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Flash Chat iOS13
+//
+//  Created by user on 15.10.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
